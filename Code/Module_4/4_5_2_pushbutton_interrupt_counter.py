2c3a92368a9e4c72fc6de687725f82711e58207b
Rui Santos <ee11077@fe.up.pt>
2024-02-21 10:15:31 +0000

    Update 4_5_2_pushbutton_interrupt_counter.py

--- Code/Module_4/4_5_2_pushbutton_interrupt_counter.py
+++ Code/Module_4/4_5_2_pushbutton_interrupt_counter.py
@@ -4,7 +4,7 @@
 from machine import Pin
 
 button = Pin(21, Pin.IN, Pin.PULL_DOWN)
-counter = 0; # Initialize the button press count
+counter = 0  # Initialize the button press count
 
 def button_pressed(pin):
     global counter # Declare variable as global
