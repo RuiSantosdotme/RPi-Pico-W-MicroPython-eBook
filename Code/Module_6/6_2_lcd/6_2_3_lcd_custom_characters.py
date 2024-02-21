78361e573e97c6ff1ac572b8ac42884d4502a6e0
Rui Santos <randomnerdtutorials@gmail.com>
2024-02-21 18:15:06 +0000

    Update 6_2_3_lcd_custom_characters.py

--- Code/Module_6/6_2_lcd/6_2_3_lcd_custom_characters.py
+++ Code/Module_6/6_2_lcd/6_2_3_lcd_custom_characters.py
@@ -16,24 +16,24 @@ lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 # Create custom characters here: https://maxpromer.github.io/LCD-Character-Creator/
 thermometer = bytearray([0x04, 0x0A, 0x0A, 0x0A, 0x0A, 0x1B, 0x1F, 0x0E])
-lcd.custom_char (0, thermometer)
+lcd.custom_char(0, thermometer)
 
 umbrella = bytearray([0x00, 0x04, 0x0E, 0x1F, 0x04, 0x04, 0x014, 0x0C])
-lcd.custom_char (1, umbrella)
+lcd.custom_char(1, umbrella)
 
 heart = bytearray([0x00, 0x0A, 0x1F, 0x1F, 0x0E, 0x04, 0x00, 0x00])
-lcd.custom_char (2, heart)
+lcd.custom_char(2, heart)
 
 try:
     lcd.putstr("Characters")
-    lcd.move_to(0,1)
+    lcd.move_to(0, 1)
     # Display thermometer
     lcd.putchar(chr(0))
     # Display umbrella
-    lcd.move_to(2,1)
+    lcd.move_to(2, 1)
     lcd.putchar(chr(1))
     # Display heart
-    lcd.move_to(4,1)
+    lcd.move_to(4, 1)
     lcd.putchar(chr(2))
  
 except KeyboardInterrupt:
