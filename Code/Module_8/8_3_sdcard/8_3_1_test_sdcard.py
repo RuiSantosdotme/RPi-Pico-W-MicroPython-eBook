4b10edcadd9d968e8363cf023d444235d6a1bf18
Rui Santos <randomnerdtutorials@gmail.com>
2024-02-22 14:41:47 +0000

    Update 8_3_1_test_sdcard.py

--- Code/Module_8/8_3_sdcard/8_3_1_test_sdcard.py
+++ Code/Module_8/8_3_sdcard/8_3_1_test_sdcard.py
@@ -13,12 +13,14 @@ CS_PIN = 5
 SD_MOUNT_PATH = '/sd'
 
 try:
+    # Init SPI communication
     spi = SPI(SPI_BUS,sck=Pin(SCK_PIN), mosi=Pin(MOSI_PIN), miso=Pin(MISO_PIN))
     cs = Pin(CS_PIN)
     sd = sdcard.SDCard(spi, cs)
+    # Mount microSD card
     os.mount(sd, SD_MOUNT_PATH)
+    # List files on the microSD card
     print(os.listdir(SD_MOUNT_PATH))
 
-    
 except Exception as e:
     print('An error occurred:', e)
