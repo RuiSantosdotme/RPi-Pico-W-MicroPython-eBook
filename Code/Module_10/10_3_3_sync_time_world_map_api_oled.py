e0040735c7eb4a40d551203eb4962d625481e72a
Rui Santos <randomnerdtutorials@gmail.com>
2024-02-25 17:10:22 +0000

    Update 10_3_3_sync_time_world_map_api_oled.py

--- Code/Module_10/10_3_3_sync_time_world_map_api_oled.py
+++ Code/Module_10/10_3_3_sync_time_world_map_api_oled.py
@@ -16,7 +16,7 @@ i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 # Set up the OLED display parameters
 oled_width = 128
 oled_height = 64
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c, addr=0x3c)
 
 days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
