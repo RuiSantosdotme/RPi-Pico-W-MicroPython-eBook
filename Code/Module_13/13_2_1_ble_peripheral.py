4993e1360874f3436187ae227a8bea32f5e30a37
Rui Santos <randomnerdtutorials@gmail.com>
2024-03-16 11:07:48 +0000

    Update 13_2_1_ble_peripheral.py

--- Code/Module_13/13_2_1_ble_peripheral.py
+++ Code/Module_13/13_2_1_ble_peripheral.py
@@ -8,43 +8,46 @@ import bluetooth
 import struct
 from picozero import pico_temp_sensor
 
-# org.bluetooth.service.environmental_sensing
-_ENV_SENSE_UUID = bluetooth.UUID('3ac77742-6924-4289-925a-6ad86dfeb618')
+#org.bluetooth.service.environmental_sensing
+_ENV_SENSE_UUID = bluetooth.UUID(0x181A)
 # org.bluetooth.characteristic.temperature
-_ENV_SENSE_TEMP_UUID = bluetooth.UUID('52fcd45e-c477-47d3-9dcf-cc9813795b42')
+_ENV_SENSE_TEMP_UUID = bluetooth.UUID(0x2A6E)
 # org.bluetooth.characteristic.gap.appearance.xml
 _ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
-
 # How frequently to send advertising beacons.
 _ADV_INTERVAL_MS = 250_000
 
 # Register GATT server.
 temp_service = aioble.Service(_ENV_SENSE_UUID)
 temp_characteristic = aioble.Characteristic(
-    temp_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True
-)
+temp_service, _ENV_SENSE_TEMP_UUID, read=True, notify=True)
 aioble.register_services(temp_service)
 
-# Get temperature and update characteristic
-async def write_characteristic_task():
-    while True:
-        led_state = 'True'
-        temp_characteristic.write(led_state, send_update=True)
-        print(led_state)
-        await asyncio.sleep_ms(1000)
+# Helper to encode the temperature characteristic encoding
+# (sint16, hundredths of a degree).
+def _encode_temperature(temp_deg_c):
+    return struct.pack("<h", int(temp_deg_c * 100))
 
+# Get temperature and update characteristic
+async def sensor_task():
+    while True:
+        temperature = pico_temp_sensor.temp
+        temp_characteristic.write(_encode_temperature(temperature), send_update=True)
+        print(temperature)
+        await asyncio.sleep_ms(1000)
+        
 # Serially wait for connections. Don't advertise while a central is connected.
 async def peripheral_task():
     while True:
-        print("Start advertising")
         try:
             async with await aioble.advertise(
                 _ADV_INTERVAL_MS,
                 name="RPi-Pico",
                 services=[_ENV_SENSE_UUID],
-            ) as connection:
-                print("Connection from", connection.device)
-                await connection.disconnected()
+                appearance=_ADV_APPEARANCE_GENERIC_THERMOMETER,
+                ) as connection:
+                    print("Connection from", connection.device)
+                    await connection.disconnected()
         except asyncio.CancelledError:
             # Catch the CancelledError
             print("Peripheral task cancelled")
@@ -54,10 +57,10 @@ async def peripheral_task():
             # Ensure the loop continues to the next iteration
             await asyncio.sleep_ms(100)
 
-# Run both tasks.
+# Run both tasks
 async def main():
-    t1 = asyncio.create_task(write_characteristic_task())
+    t1 = asyncio.create_task(sensor_task())
     t2 = asyncio.create_task(peripheral_task())
     await asyncio.gather(t1, t2)
-
+    
 asyncio.run(main())
